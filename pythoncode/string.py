dd5b1ebbd885b065b9e7e019f28620839e781aaa
GoodFlag <18637273636@163.com>
2022-11-13 22:16:00 +0800

    我的python练习

---
+++ pythoncode/string.py
@@ -0,0 +1,19 @@
+print("hello,python world!")
+message="John"
+print("hello,"+message)
+name="John Smith"
+print(name.upper())
+print(name.lower())
+full_name='abc'
+second_name='tommas'
+print((full_name + ' '+ second_name).title())
+print("\n\tPython,\n\tGo,\n\tRust")
+name=' python '
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+print(2*3)
+a=26
+b=26
+print(a+a*b+a/b)
+print('happy'+' '+str(51)+' '+"birthday")
